4ba4b6c2704c78969ee39c4570f3a4ce9ddea20f
haruya <kuroiwaharuya@gmail.com>
2025-11-07 20:32:43 +0900

    タスク作成機能

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/items/")
+def add_item(name: str, kind: str, date: str, score: int):
+    return "取得完了"
